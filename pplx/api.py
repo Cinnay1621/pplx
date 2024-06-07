75227d0034ec70c242cae475156f766f4d9fac77
Yannic <52500754+Cinnay1621@users.noreply.github.com>
2024-06-08 00:48:29 +0200

    Add files via upload

--- pplx/api.py
+++ pplx/api.py
@@ -9,6 +9,7 @@ class PerplexityAPI:
             "content-type": "application/json",
             "Authorization": f"Bearer {self.api_key}"
         }
+        self.last_response = None
 
     def create_payload(self, model, messages, **kwargs):
         payload = {
@@ -21,4 +22,12 @@ class PerplexityAPI:
     def send_request(self, model, messages, **kwargs):
         payload = self.create_payload(model, messages, **kwargs)
         response = requests.post(self.base_url, json=payload, headers=self.headers)
-        return response.json()
+        json_data = response.json()
+        self.last_response = json_data
+        return json_data
+    
+    def get_response(self, json_data=None):
+        if not json_data: json_data = self.last_response
+        if not json_data: return None
+        else: return json_data['choices'][0]['message']['content']
+    
